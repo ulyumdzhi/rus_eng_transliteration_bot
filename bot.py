72c788c74483334e0af45ada2ad727c83a03f7f0
ulyumdzhiev <ulyumdzhiev@gmail.com>
2022-11-24 23:03:45 +0300

    update

--- bot.py
+++ bot.py
@@ -3,7 +3,11 @@ import logging
 
 from aiogram import Bot, Dispatcher, executor, types
 
-from trans import transliteration_dict
+from trans import FAIL
+from utils import trans_checker
+
+
+TOKEN = os.getenv("TOKEN")
 
 
 HELLO = """Если вы покупаете авиабилет на внутренний паспорт или свидетельство \
@@ -11,13 +15,14 @@ HELLO = """Если вы покупаете авиабилет на внутре
 транслитерации, установленные Приказом МИД России от 12.02.2020 № 2113. \
 
 Отправь мне полное ФИО и я транслитерирую их в соответствии с правилами."""
-        
-TOKEN = os.getenv("TOKEN")
 
-logging.basicConfig(level=logging.INFO)
+
+logging.basicConfig(level=logging.INFO, 
+                    filename='translit.log')
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
+
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
@@ -29,17 +34,24 @@ async def send_welcome(message: types.Message):
     await bot.send_message(user_id, HELLO)
     
 @dp.message_handler()
-async def make_echo(message):
+async def translit_this(message):
     chat_id = message.from_user.id
     fio = message.text
     logging.info(f'{fio=} from {chat_id=}')
-    translit = ''
+    translit = ' '
+    _translit = []
     try:
-        for i in fio.split():
-            translit += ' '+''.join([transliteration_dict[j.upper()] for j in i])
-            translit = translit.strip()
-    except:
-        translit = 'Не удалось транслитерировать. Попробуйте еще раз написать ФИО только на кириллице'  
+        for word in fio.split():
+            code, result = trans_checker(word)
+            if code:
+                _translit.append(result)
+            else:
+                raise ValueError(f'{word}: символ "{word[result]}"')
+        
+        translit = translit.join(_translit)
+        
+    except Exception as e:
+        translit = FAIL.format(e)
     
     logging.info(f'{translit=}')
     await bot.send_message(chat_id, translit)
